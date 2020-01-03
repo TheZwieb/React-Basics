9fda1e2f3fd21edf3ec15ac046a31ed815822f08
Austin <thezwieb@gmail.com>
2020-01-03 16:15:36 -0600

    Updates

---
+++ precache-manifest.86ba5be8fde76143153ebef4d78e2f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4055c83b91652020005ca2140d7ea4",
+    "url": "/React-Basics/index.html"
+  },
+  {
+    "revision": "893dab8bc1b69e77a2f7",
+    "url": "/React-Basics/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fefa9dca23d87667a664",
+    "url": "/React-Basics/static/js/2.c509a7d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Basics/static/js/2.c509a7d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "893dab8bc1b69e77a2f7",
+    "url": "/React-Basics/static/js/main.92196689.chunk.js"
+  },
+  {
+    "revision": "010bdbb565b983fbfc95",
+    "url": "/React-Basics/static/js/runtime-main.b3341a0f.js"
+  }
+]);
