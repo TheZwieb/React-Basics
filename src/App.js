e4605009b635696ec11c4dc6070aa703e09eb0e6
Austin <thezwieb@gmail.com>
2020-01-03 14:13:14 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Coders from './Coders';
+import AddCoder from './AddCoder';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    Coders : [
+      {name: 'Austin', age: 25, skillLevel: 'n00b', id:1 },
+      {name: 'Sydwell', age: 28, skillLevel: 'Senior', id:2 },
+      {name: 'J. Carmac', age: 49, skillLevel: 'Grand Master', id:3 },
+    ]
+  }
+  addCoder=(Coder) => {
+    Coder.id = Math.random();
+    let coders = [...this.state.Coders, Coder];
+    this.setState({
+      Coders: coders
+    })
+  }
+    render() {
+      return (
+        <div className="App">
+          <h1>React Basics</h1>
+          <p>CODERS:</p>
+          <Coders Coders={this.state.Coders}/>
+          <AddCoder AddCoder={this.addCoder}/>
+        </div>
+      );
+    }
 }
 
 export default App;
